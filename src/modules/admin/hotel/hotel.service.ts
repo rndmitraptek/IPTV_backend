fcf276288211bcd7c94fa8993462038a10fd32dc
ahmat.yusuf <ahmat.yusuf@maxsol.id>
2025-01-16 15:37:11 +0700

    update auto role on new hotel

--- src/modules/admin/hotel/hotel.service.ts
+++ src/modules/admin/hotel/hotel.service.ts
@@ -3,66 +3,96 @@ import { InjectModel } from '@nestjs/sequelize';
 import { iptv_feature } from 'src/database/iptv/iptv_feature.entity';
 import { hotelDtoInsert } from './hotel.dto';
 import * as bcrypt from 'bcrypt';
+import { role } from 'src/database/iptv/role.entity';
+import { role_menu } from 'src/database/iptv/role_menu.entity';
 
 @Injectable({ scope: Scope.REQUEST })
 export class HotelService {
-    constructor(
-        @InjectModel(iptv_feature)
-        private iptv_featureModel: typeof iptv_feature,
-    ) {}
-    
-    findAll(req:any): Promise<iptv_feature[]> {
-        try {
-            return this.iptv_featureModel.findAll({order:[['id','desc']]});            
-        } catch (error) {
-            throw error;
-        }
-    }
-    
-    findOne(id: number): Promise<iptv_feature> {
-        return this.iptv_featureModel.findOne({
-            where: {
-                id:id,
-            },
-        });
-    }
-    
-    async create(_iptv_feature: hotelDtoInsert, req:any): Promise<iptv_feature> {
-        if(req.user.is_admin ==undefined){
-            throw ('Akun anda tidak diperbolehkan menambah data ini');
-        }
-        if(req.user.is_admin==false){
-            throw ('Akun anda tidak diperbolehkan menambah data ini');
-        }
-        _iptv_feature['is_active']=true;
-        _iptv_feature['created_by']=req.user.username;
-        _iptv_feature['updated_by']=req.user.username;
-        if(_iptv_feature.pin !=undefined){
-            if(_iptv_feature.pin!=null){
-                _iptv_feature.pin = await bcrypt.hash(_iptv_feature.pin, 10);
-            }
-        }
+  constructor(
+    @InjectModel(iptv_feature)
+    private iptv_featureModel: typeof iptv_feature,
+    @InjectModel(role)
+    private _role: typeof role,
+    @InjectModel(role_menu)
+    private _role_menu: typeof role_menu,
+  ) {}
 
-        return this.iptv_featureModel.create(_iptv_feature);
+  findAll(req: any): Promise<iptv_feature[]> {
+    try {
+      return this.iptv_featureModel.findAll({ order: [['id', 'desc']] });
+    } catch (error) {
+      throw error;
     }
-    
-    async update(id: number, _iptv_feature: hotelDtoInsert,req:any): Promise<void> {
-        _iptv_feature['updated_by']=req.user.username;
-        if(_iptv_feature.pin !=undefined){
-            if(_iptv_feature.pin!=null){
-                _iptv_feature.pin = await bcrypt.hash(_iptv_feature.pin, 10);
-            }
-        }
-        
-        await this.iptv_featureModel.update(_iptv_feature, {
-            where: {
-                id:id,
-            },
-        });
+  }
+
+  findOne(id: number): Promise<iptv_feature> {
+    return this.iptv_featureModel.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async create(_iptv_feature: hotelDtoInsert, req: any): Promise<iptv_feature> {
+    if (req.user.is_admin == undefined) {
+      throw 'Akun anda tidak diperbolehkan menambah data ini';
     }
-    
-    async remove(id: number): Promise<void> {
-        const iptv_feature = await this.findOne(id);
-        await iptv_feature.destroy();
+    if (req.user.is_admin == false) {
+      throw 'Akun anda tidak diperbolehkan menambah data ini';
     }
+    _iptv_feature['is_active'] = true;
+    _iptv_feature['created_by'] = req.user.username;
+    _iptv_feature['updated_by'] = req.user.username;
+    if (_iptv_feature.pin != undefined) {
+      if (_iptv_feature.pin != null) {
+        _iptv_feature.pin = await bcrypt.hash(_iptv_feature.pin, 10);
+      }
+    }
+
+    const hotel = await this.iptv_featureModel.create(_iptv_feature);
+
+    //create role admin default
+    const role = await this._role.create({
+      role: 'admin',
+      id_hotel: hotel.id,
+    });
+
+    //create role_menu default admin
+    const menuArr = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+      22, 23, 24, 25, 26, 27, 28, 29,
+    ];
+    for (let i = 0; i < menuArr.length; i++) {
+      const roleMenu = await this._role_menu.create({
+        id_role: role.id_role,
+        id_menu: menuArr[i],
+      });
+    }
+
+    return hotel;
+  }
+
+  async update(
+    id: number,
+    _iptv_feature: hotelDtoInsert,
+    req: any,
+  ): Promise<void> {
+    _iptv_feature['updated_by'] = req.user.username;
+    if (_iptv_feature.pin != undefined) {
+      if (_iptv_feature.pin != null) {
+        _iptv_feature.pin = await bcrypt.hash(_iptv_feature.pin, 10);
+      }
+    }
+
+    await this.iptv_featureModel.update(_iptv_feature, {
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async remove(id: number): Promise<void> {
+    const iptv_feature = await this.findOne(id);
+    await iptv_feature.destroy();
+  }
 }
--- src/modules/admin/user-cms/user-cms.service.ts
+++ src/modules/admin/user-cms/user-cms.service.ts
@@ -10,84 +10,94 @@ import { usersCmsDtoInsert, usersCmsDtoUpdate } from './user-cms.dto';
 
 @Injectable({ scope: Scope.REQUEST })
 export class UserCmsService {
-    constructor(
-        @InjectModel(users)
-        private userModel: typeof users,
-        @InjectModel(role)
-        private roleModel: typeof role,
-        @InjectModel(iptv_feature)
-        private iptv_featureModel: typeof iptv_feature,
-        private sequelize:Sequelize
-    ) {}
-    
-    findAll(req:any): Promise<users[]> {
-        try {
-            return this.userModel.findAll({
-                attributes:[
-                    'id_user',
-                    'nama',
-                    'username',
-                    'id_role',
-                    'is_active',
-                    'is_admin',
-                    'id_hotel',
-                    [this.sequelize.col('role.role'),'nama_role'],
-                    [this.sequelize.col('hotel.title_hotel'),'nama_hotel'],
-                ],
-                include:[
-                    {
-                        attributes:[],
-                        model:role,
-                        as:'role'
-                    },
-                    {
-                        attributes:[],
-                        model:iptv_feature,
-                        as:'hotel'
-                    }
-                ],
-                order:[['id_user','desc']]
-            });            
-        } catch (error) {
-            throw error;
-        }
+  constructor(
+    @InjectModel(users)
+    private userModel: typeof users,
+    @InjectModel(role)
+    private roleModel: typeof role,
+    @InjectModel(iptv_feature)
+    private iptv_featureModel: typeof iptv_feature,
+    private sequelize: Sequelize,
+  ) {}
+
+  findAll(req: any): Promise<users[]> {
+    try {
+      return this.userModel.findAll({
+        attributes: [
+          'id_user',
+          'nama',
+          'username',
+          'id_role',
+          'is_active',
+          'is_admin',
+          'id_hotel',
+          [this.sequelize.col('role.role'), 'nama_role'],
+          [this.sequelize.col('hotel.title_hotel'), 'nama_hotel'],
+        ],
+        include: [
+          {
+            attributes: [],
+            model: role,
+            as: 'role',
+          },
+          {
+            attributes: [],
+            model: iptv_feature,
+            as: 'hotel',
+          },
+        ],
+        order: [['id_user', 'desc']],
+      });
+    } catch (error) {
+      throw error;
     }
-    
-    findOne(id: number): Promise<users> {
-        return this.userModel.findOne({
-            where: {
-                id_user:id,
-            },
-        });
+  }
+
+  findOne(id: number): Promise<users> {
+    return this.userModel.findOne({
+      where: {
+        id_user: id,
+      },
+    });
+  }
+
+  async create(user: usersCmsDtoInsert, req: any): Promise<users> {
+    const cekRoleAdmin = await this.roleModel.findOne({
+      where: { id_hotel: user.id_hotel, role: 'admin' },
+    });
+    if (cekRoleAdmin && user.id_role == 0) {
+      user.id_role = cekRoleAdmin.id_role;
     }
-    
-    async create(user: usersCmsDtoInsert,req:any): Promise<users> {
-        user.password = await bcrypt.hash(user.password, 10);
-        user['is_admin']=false;
-        user['is_active']=true;
-        return this.userModel.create(user);
+
+    user.password = await bcrypt.hash(user.password, 10);
+    user['is_admin'] = false;
+    user['is_active'] = true;
+    return this.userModel.create(user);
+  }
+
+  async update(uuid: number, user: usersCmsDtoUpdate): Promise<void> {
+    if (user.password) {
+      user.password = await bcrypt.hash(user.password, 10);
     }
-    
-    async update(uuid: number, user: usersCmsDtoUpdate): Promise<void> {
-        if(user.password){
-            user.password = await bcrypt.hash(user.password, 10);
-        }
-        await this.userModel.update(user, {
-            where: {
-                id_user:uuid,
-            },
-        });
+    await this.userModel.update(user, {
+      where: {
+        id_user: uuid,
+      },
+    });
+  }
+
+  async remove(uuid: number): Promise<void> {
+    const user = await this.findOne(uuid);
+    if (user == null) {
+      throw 'Data tidak ditemukan';
     }
-    
-    async remove(uuid: number): Promise<void> {
-        const user = await this.findOne(uuid);
-        if(user==null){
-            throw('Data tidak ditemukan');
-        }
-        await this.userModel.update({is_active:false}, {
-            where: {
-                id_user:uuid,
-            },
-        });
-    }
+    await this.userModel.update(
+      { is_active: false },
+      {
+        where: {
+          id_user: uuid,
+        },
+      },
+    );
+  }
 }
