ae1e574bce44c870957e9697d9e3ea3fcc0f924b
ahmat.yusuf <ahmat.yusuf@maxsol.id>
2025-03-06 15:49:52 +0700

    update ent

--- src/modules/cms/entertainment/entertainment.service.ts
+++ src/modules/cms/entertainment/entertainment.service.ts
@@ -2,61 +2,101 @@ import { Injectable, Scope } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { entertainment } from 'src/database/iptv/entertainment.entity';
 import { entertainmentDtoInsert } from './entertainment.dto';
+import { ApkService } from 'src/modules/tv/apk/apk.service';
 
 @Injectable({ scope: Scope.REQUEST })
 export class EntertainmentService {
-    constructor(
-        @InjectModel(entertainment)
-        private entertainmentModel: typeof entertainment,
-    ) {}
+  constructor(
+    @InjectModel(entertainment)
+    private entertainmentModel: typeof entertainment,
+    private _ApkService: ApkService,
+  ) {}
 
-    async updateStatusActive(id_app:number):Promise<entertainment>{
-        try {
-            let data = await this.entertainmentModel.findOne({
-                where:{
-                    id_app:id_app
-                }
-            });
-            data.update({
-                is_active:!data.is_active
-            })
-            return data;
-        } catch (error) {
-            throw error;
-        }
+  async updateStatusActive(id_app: number, req: any): Promise<entertainment> {
+    try {
+      let data = await this.entertainmentModel.findOne({
+        where: {
+          id_app: id_app,
+        },
+      });
+      data.update({
+        is_active: !data.is_active,
+      });
+
+      if (req.user.id_hotel != undefined) {
+        let sendWS = await this._ApkService.sendWebsocketData(
+          req,
+          'entertainment',
+          'update',
+        );
+      }
+      return data;
+    } catch (error) {
+      throw error;
     }
-    
-    findAll(): Promise<entertainment[]> {
-        try {
-            return this.entertainmentModel.findAll();            
-        } catch (error) {
-            throw error;
-        }
+  }
+
+  findAll(): Promise<entertainment[]> {
+    try {
+      return this.entertainmentModel.findAll();
+    } catch (error) {
+      throw error;
     }
-    
-    findOne(id_app: number): Promise<entertainment> {
-        return this.entertainmentModel.findOne({
-            where: {
-                id_app:id_app,
-            },
-        });
+  }
+
+  findOne(id_app: number): Promise<entertainment> {
+    return this.entertainmentModel.findOne({
+      where: {
+        id_app: id_app,
+      },
+    });
+  }
+
+  async create(
+    _entertainment: entertainmentDtoInsert,
+    req: any,
+  ): Promise<entertainment> {
+    const insert = await this.entertainmentModel.create(_entertainment);
+    if (req.user.id_hotel != undefined) {
+      let sendWS = await this._ApkService.sendWebsocketData(
+        req,
+        'entertainment',
+        'create',
+      );
     }
-    
-    async create(_entertainment: entertainmentDtoInsert): Promise<entertainment> {
-        return this.entertainmentModel.create(_entertainment);
+    return insert;
+  }
+
+  async update(
+    id_app: number,
+    _entertainment: entertainmentDtoInsert,
+    req: any,
+  ): Promise<void> {
+    await this.entertainmentModel.update(_entertainment, {
+      where: {
+        id_app: id_app,
+      },
+    });
+
+    if (req.user.id_hotel != undefined) {
+      let sendWS = await this._ApkService.sendWebsocketData(
+        req,
+        'entertainment',
+        'update',
+      );
     }
-    
-    async update(id_app: number, _entertainment: entertainmentDtoInsert): Promise<void> {
-        await this.entertainmentModel.update(_entertainment, {
-            where: {
-                id_app:id_app,
-            },
-        });
+  }
+
+  async remove(id_app: number, req: any): Promise<void> {
+    const entertainment = await this.findOne(id_app);
+    await entertainment.destroy();
+
+    if (req.user.id_hotel != undefined) {
+      let sendWS = await this._ApkService.sendWebsocketData(
+        req,
+        'entertainment',
+        'delete',
+      );
     }
-    
-    async remove(id_app: number): Promise<void> {
-        const entertainment = await this.findOne(id_app);
-        await entertainment.destroy();
-    }
+  }
 }
-            
