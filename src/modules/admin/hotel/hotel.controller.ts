d33c93f2748e3be3cb99ffc26600efd931b256c5
ahmat.yusuf <ahmat.yusuf@maxsol.id>
2025-06-26 10:21:19 +0700

    update

--- src/modules/admin/hotel/hotel.controller.ts
+++ src/modules/admin/hotel/hotel.controller.ts
@@ -86,8 +86,8 @@ export class HotelController {
   }
 
   @Put('update_is_stream')
-  @UseGuards(JwtAuthGuard)
-  @ApiBearerAuth('access-token')
+  //   @UseGuards(JwtAuthGuard)
+  //   @ApiBearerAuth('access-token')
   @ApiOperation({ summary: 'Update is_stream data hotel' })
   update_is_stream(@Body() body: updateIsStreamHotel, @Req() req: Request) {
     return this._hotelService.updateIsStream(body);
